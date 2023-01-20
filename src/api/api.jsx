3bcc7da61f0818f4279ee276bef4a5f8d7299aac
vincent <vincentpalomo@yahoo.com>
2023-01-20 15:56:27 -0600

    added fetch for request for all users

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -4,5 +4,12 @@ const APIURL = 'https://juicebox-gu05.onrender.com/api';
 export const fetchAllPosts = async () => {
   const res = await fetch(`${APIURL}/posts`);
   const json = await res.json();
+  return json.posts;
+};
+
+// fetch all users
+export const fetchAllUsers = async () => {
+  const res = await fetch(`${APIURL}/users`);
+  const json = await res.json();
   return json;
 };
