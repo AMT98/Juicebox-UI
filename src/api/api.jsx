0d0deae81f046490a102c41a0c462065297606f6
vincent <vincentpalomo@yahoo.com>
2023-01-20 16:25:03 -0600

    added fetch for login, auth/loggedinuser, & create post

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -31,3 +31,51 @@ export const fetchRegister = async (username, password, name, location) => {
   const json = await res.json();
   return json;
 };
+
+// login user
+export const fetchLogin = async (username, password) => {
+  const res = await fetch(`${APIURL}/users/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username: `${username}`,
+      password: `${password}`,
+    }),
+  });
+  const json = await res.json();
+  return json;
+};
+
+// authorization check & logged in user
+export const fetchLoggedInUser = async (token) => {
+  const res = await fetch(`${APIURL}/posts`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const json = await res.json();
+  return json;
+};
+
+// create post
+export const fetchAddPost = async (token, title, content, tags) => {
+  const res = await fetch(`${APIURL}/posts`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      body: {
+        title: `${title}`,
+        content: `${content}`,
+        tags: `${tags}`,
+      },
+    }),
+  });
+  const json = await res.json();
+  return json;
+};
