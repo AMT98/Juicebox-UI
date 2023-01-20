aebf8ab16fa3ae347ca89b36ba39f868c18cb084
vincent <vincentpalomo@yahoo.com>
2023-01-20 17:29:18 -0600

    finished login form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import NavBar from './NavBar';
 import Register from './Register';
+import Login from './Login';
 
 const App = () => {
   return (
     <>
       <NavBar />
-      <Register />
+      {/* <Register /> */}
+      <Login />
     </>
   );
 };
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { fetchLogin } from '../api/api';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setUsername('');
+    setPassword('');
+
+    try {
+      const login = await fetchLogin(username, password);
+      if (!login.success) {
+        setErrorMessage(login.error.message);
+      }
+    } catch (error) {
+      console.error('error in login', error);
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Username:</label>
+        <input
+          type='text'
+          name='username'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label>Password:</label>
+        <input
+          type='text'
+          name='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <span>
+          <button type='submit'>Login</button>
+        </span>
+        <p>{errorMessage}</p>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
