b1a949976e60f27dde258c6e6a7e281af87d7d58
vincent <vincentpalomo@yahoo.com>
2023-01-20 22:14:18 -0600

    html for register form complete

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import { fetchRegister } from '../api/api';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   const [username, setUsername] = useState('');
@@ -26,60 +27,85 @@ const Register = () => {
     }
   };
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div className='mb-3'>
-          <label className='form-label'>Username:</label>
-          <input
-            className='form-control'
-            type='text'
-            name='username'
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
+    <section className='vh-100'>
+      <div className='container py-5 h-100'>
+        <div className='row d-flex justify-content-center align-items-center h-100'>
+          <div className='col-12 col-md-8 col-lg-6 col-xl-5'>
+            <div className='card bg-dark text-white'>
+              <div className='card-body p-5 text-center'>
+                <h2 className='fw-bold mb-2 text-uppercase'>Register</h2>
+                <p className='text-white-50 mb-5'>
+                  Please enter your username, password, name and location!
+                </p>
+                <form onSubmit={handleSubmit}>
+                  <div className='form-outline form-white mb-4'>
+                    <label className='form-label'>Username:</label>
+                    <input
+                      className='form-control form-control-lg'
+                      type='text'
+                      name='username'
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className='form-outline form-white mb-4'>
+                    <label className='form-label'>Password:</label>
+                    <input
+                      className='form-control form-control-lg'
+                      type='text'
+                      name='password'
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className='form-outline form-white mb-4'>
+                    <label className='form-label'>Name:</label>
+                    <input
+                      className='form-control form-control-lg'
+                      type='text'
+                      name='name'
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className='form-outline form-white mb-4'>
+                    <label className='form-label'>Location:</label>
+                    <input
+                      className='form-control form-control-lg'
+                      type='text'
+                      name='location'
+                      value={location}
+                      onChange={(e) => setLocation(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <span>
+                    <button
+                      className='btn btn-outline-light btn-lg px-5'
+                      type='submit'
+                    >
+                      Create Account
+                    </button>
+                  </span>
+                  <p>{errorMessage}</p>
+                </form>
+                <div>
+                  <p className='mb-0'>
+                    Have an account?{' '}
+                    <Link className='text-white-50 fw-bold' to='/login'>
+                      Login
+                    </Link>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className='mb-3'>
-          <label className='form-label'>Password:</label>
-          <input
-            className='form-control'
-            type='text'
-            name='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div className='mb-3'>
-          <label className='form-label'>Name:</label>
-          <input
-            className='form-control'
-            type='text'
-            name='name'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
-        <div className='mb-3'>
-          <label className='form-label'>Location:</label>
-          <input
-            className='form-control'
-            type='text'
-            name='location'
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-            required
-          />
-        </div>
-        <span>
-          <button className='btn btn-primary' type='submit'>
-            Create Account
-          </button>
-        </span>
-        <p>{errorMessage}</p>
-      </form>
-    </div>
+      </div>
+    </section>
   );
 };
 
