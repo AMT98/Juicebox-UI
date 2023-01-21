134f5a5baa1398fb1322b60f25dbdbb203e079e8
Vincent Palomo <vincentpalomo@yahoo.com>
2023-01-21 10:25:33 -0600

    cards now mapping posts but tags are not working at the moment

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchAllPosts } from '../api/api';
 
 const Posts = () => {
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = async () => {
+    try {
+      const post = await fetchAllPosts();
+      console.log(post);
+      setPosts(post);
+    } catch (error) {
+      console.error('error in post fetchpost', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <section className='d-md-flex justify-content-md-center align-items-md-center'>
-      <div
-        className='card text-light bg-dark border rounded border-2 shadow-lg bounce animated'
-        style={{ width: '512px' }}
-      >
-        <div className='card-body' style={{ width: '512px' }}>
-          <h4 className='card-title'>Title</h4>
-          <p className='card-text'>
-            Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo
-            odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non
-            mi porta gravida at eget metus.
-          </p>
-          <p className='text-white-50'>#tags</p>
-        </div>
-      </div>
+      {posts.map((post) => {
+        return (
+          <div
+            className='card text-light bg-dark border rounded border-2 shadow-lg bounce animated'
+            style={{ width: '512px' }}
+            key={post.id}
+          >
+            <div className='card-body' style={{ width: '512px' }}>
+              <h4 className='card-title'>{post.title}</h4>
+              <p className='card-text'>{post.content}</p>
+              <p className='text-white-50'>#tags</p>
+            </div>
+          </div>
+        );
+      })}
     </section>
   );
 };
