b2d9ad5e5474ac57216000dc12a27289b15d9ff7
Vincent Palomo <vincentpalomo@yahoo.com>
2023-01-21 11:06:26 -0600

    fixed some styling on the card

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -19,23 +19,29 @@ const Posts = () => {
   }, []);
 
   return (
-    <section className='d-md-flex justify-content-md-center align-items-md-center'>
-      {posts.map((post) => {
-        return (
-          <div
-            className='card text-light bg-dark border rounded border-2 shadow-lg bounce animated'
-            style={{ width: '512px' }}
-            key={post.id}
-          >
-            <div className='card-body' style={{ width: '512px' }}>
-              <h4 className='card-title'>{post.title}</h4>
-              <p className='card-text'>{post.content}</p>
-              <p className='text-white-50'>#tags</p>
-            </div>
-          </div>
-        );
-      })}
-    </section>
+    <div className='container d-md-flex justify-content-md-center align-items-md-center my-3'>
+      <div className='row'>
+        <div className='col'>
+          {posts.map((post) => {
+            return (
+              <div
+                className='card text-light bg-dark border rounded border-2 shadow-lg bounce animated col'
+                style={{ width: '512px' }}
+                key={post.id}
+              >
+                <div className='card-body' style={{ width: '512px' }}>
+                  <h4 className='card-title'>{post.author.username}</h4>
+                  <p className='card-text'>{post.content}</p>
+                  <p className='text-white-50'>
+                    {post.tags[0].name} {post.tags[1].name}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
   );
 };
 
