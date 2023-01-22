2d6fed042e68ab3b95445b41ea892b644d4a41f8
AMT98 <aswinmalla12@gmail.com>
2023-01-22 16:02:40 -0700

    Add functional search bar

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 import { fetchAllPosts, fetchDeletePost } from "../api/api";
 import AddPost from "./AddPost";
 
-const token = localStorage.getItem('token')
+const token = localStorage.getItem("token");
 const Posts = () => {
   const [posts, setPosts] = useState([]);
-
+  const [searchTerm, setSearchTerm] = useState("");
   const fetchPosts = async () => {
     try {
       const post = await fetchAllPosts();
@@ -19,12 +19,12 @@ const Posts = () => {
   const handleDeletePost = async (postID, token) => {
     try {
       const deletePost = await fetchDeletePost(postID, token);
-     console.log(deletePost)
+      console.log(deletePost);
       fetchPosts();
     } catch (error) {
-      console.error('error in post delete fn', error);
+      console.error("error in post delete fn", error);
     }
-  }
+  };
 
   useEffect(() => {
     fetchPosts();
@@ -34,31 +34,63 @@ const Posts = () => {
     <div>
       <div className="container d-md-flex justify-content-md-center align-items-md-center my-3">
         <h1>POSTS</h1>
-        <AddPost />
+        <form>
+          <AddPost />
+          <input
+            type="search"
+            placeholder="Search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          ></input>
+        </form>
       </div>
       <div className="container d-md-flex justify-content-md-center align-items-md-center my-3">
         <div className="row">
           <div className="col">
-            {posts.map((post) => {
-              return (
-                <div
-                  className="card text-light bg-dark border rounded border-2 shadow-lg bounce animated col"
-                  style={{ width: "512px" }}
-                  key={post.id}
-                >
-                  <div className="card-body" style={{ width: "512px" }}>
-                    <h4 className="card-title">{post.author.username}</h4>
-                    <p className="card-text">{post.content}</p>
-                    <p className="text-white-50">
-                      {post.tags[0].name} {post.tags[1].name}
-                    </p>
-                    <button
-                    onClick={() => handleDeletePost(post.id, token)}
-                    >Delete</button>
+            {posts
+              .filter((value) => {
+                if (searchTerm === "") {
+                  return value;
+                } else if (
+                  value.author.username.toLowerCase().includes(searchTerm.toLowerCase())
+                ) {
+                  return value;
+                } else if (
+                  value.content
+                    .toLowerCase()
+                    .includes(searchTerm.toLowerCase())
+                ) {
+                  return value;
+                }
+                //  else if (
+                //   value.location
+                //     .toLowerCase()
+                //     .includes(searchTerm.toLowerCase())
+                // ) 
+                // {
+                //   return value;
+                // }
+              })
+              .map((post) => {
+                return (
+                  <div
+                    className="card text-light bg-dark border rounded border-2 shadow-lg bounce animated col"
+                    style={{ width: "512px" }}
+                    key={post.id}
+                  >
+                    <div className="card-body" style={{ width: "512px" }}>
+                      <h4 className="card-title">{post.author.username}</h4>
+                      <p className="card-text">{post.content}</p>
+                      <p className="text-white-50">
+                        {post.tags[0].name} {post.tags[1].name}
+                      </p>
+                      <button onClick={() => handleDeletePost(post.id, token)}>
+                        Delete
+                      </button>
+                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
           </div>
         </div>
       </div>
